4a34c1dc7e4fd91076816ef62d090698018039c5
Hamar84 <61242531+Hamar84@users.noreply.github.com>
2020-05-25 18:56:48 +0200

    J05

--- CoronaStat/src/CoronaStat/Main.java
+++ CoronaStat/src/CoronaStat/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -11,10 +12,15 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("coronaStat.fxml"));
-        primaryStage.setTitle("CoronaStat");
-        primaryStage.setScene(new Scene(root, 800, 600));
+
+        root.setId("pane");
+        primaryStage.setTitle("Covid Statistics");
+        primaryStage.setScene(new Scene(root, 1000, 500));
         primaryStage.show();
+
+
     }
+
 
 
     public static void main(String[] args) {
